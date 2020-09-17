3a1c66157614c5995e4285112067a64b9e123960
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2020-09-16 21:57:43 -0400

    Added initial password length user prompt code

--- script.js
+++ script.js
@@ -10,7 +10,22 @@ function writePassword() {
 }
 
 function generatePassword() {
-  return "A string for the password!";
+  //set initial password length to 0 to initial while loop
+  var passwordLength = 0;
+
+  //while loop checks to see if password length is the proper size and continues to ask the user for the correct size until passwordLength meets size requirements
+
+  while (passwordLength < 8 || passwordLength > 128) {
+    // Prompts user for the password length
+    passwordLength = prompt(
+      "What length of password would you like to generate?"
+    );
+    if (passwordLength < 8 || passwordLength > 128) {
+      alert(
+        "Password length must be longer than 8 characters and less than 129 characters"
+      );
+    } else return "A string for the password!";
+  }
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
