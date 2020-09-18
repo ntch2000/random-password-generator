91c8de4668e62b329f73931f04193799a822c698
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2020-09-18 09:09:42 -0400

    Completed intial code for generating a random password with comments

--- script.js
+++ script.js
@@ -31,6 +31,8 @@ function generatePassword() {
   // boolean variable for checking if at least one character type was chosen for the password
   var characterChoice = false;
 
+  var finalPasswordString = "";
+
   //while loop checks to see if password length is the proper size and continues to ask the user for the correct size until passwordLength meets size requirements
 
   while (passwordLength < 8 || passwordLength > 128) {
@@ -101,12 +103,33 @@ function generatePassword() {
 
   //console.log(`Character chosen: ${characterChoice}`);
   //console.log(`A ${passwordLength} size string for the password!`);
-  console.log(passwordCharSet);
-  console.log(passwordCharSet.length);
+  // console.log(passwordCharSet);
+  // console.log(passwordCharSet.length);
 
   //console.log(lowerCaseCharString.length);
   //console.log(upperCaseCharString.length);
   //console.log(specialCharString.length);
+
+  //var test = Math.floor(Math.random() * passwordCharSet.length);
+
+  // sets the upper bounds of random number generator to the length of the password character set + 1.
+  var numCharacterOptions = passwordCharSet.length + 1;
+
+  // initiate the index of the password character set string
+  var passwordCharIndex;
+  //console.log(test);
+
+  // this loops through the length of the user desired password
+  for (i = 0; i < passwordLength; i++) {
+    // generates a random number and sets it to the index to pick a character
+    passwordCharIndex = Math.floor(Math.random() * numCharacterOptions);
+    console.log(passwordCharIndex);
+
+    //the final password string is built by adding a random character from the password character set to the final string. The random character is based on a random index that is chosen
+    finalPasswordString += passwordCharSet[passwordCharIndex];
+  }
+  // returns the password string to the page based on the user requirements of length, and type of character
+  return finalPasswordString;
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
